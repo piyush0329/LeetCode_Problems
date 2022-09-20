f9fa6595e802090343c35bb13cb658c0b6e55f1d
Piyush Kumar Upadhyay <68208324+piyush0329@users.noreply.github.com>
2022-09-20 22:53:33 +0530

    Time: 7 ms (85.27%), Space: 42.4 MB (84.93%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.java
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -1,40 +1,40 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        HashMap<Character,Character> pair =new HashMap<>();
-        // boolean ans = false;
-        // for(int i=0;i<s.length();i++){
-        //     if(map.containsKey(s.charAt(i))){
-        //         if(map.get(s.charAt(i))==s.charAt(i)){
-        //             continue;
-        //         }
-        //         else{
-        //             return false;
-        //         }
-        //     }else{
-        //        if(map.containsValue(t.charAt(i))){
-        //            return false;
-        //        }
-        //          map.put(s.charAt(i),s.charAt(i));
-        //     }
-        // }
-        // return true;
+        HashMap<Character,Character> map =new HashMap<>();
+        boolean ans = false;
+        for(int i=0;i<s.length();i++){
+            if(map.containsKey(s.charAt(i))){
+                if(map.get(s.charAt(i))==t.charAt(i)){
+                    continue;
+                }
+                else{
+                    return false;
+                }
+            }else{
+               if(map.containsValue(t.charAt(i))){
+                   return false;
+               }
+                 map.put(s.charAt(i),t.charAt(i));
+            }
+        }
+        return true;
         
-         for(int i = 0 ; i<s.length(); i++)
-    {
-        if(pair.containsKey(s.charAt(i)))
-        {
-            if(pair.get(s.charAt(i))==t.charAt(i))
-                continue;
-            else
-                return false;
-        }
-        else
-        {
-            if(pair.containsValue(t.charAt(i)))
-                return false;
-            pair.put(s.charAt(i),t.charAt(i));
-        }
-    }
-    return true;
+    //      for(int i = 0 ; i<s.length(); i++)
+    // {
+    //     if(pair.containsKey(s.charAt(i)))
+    //     {
+    //         if(pair.get(s.charAt(i))==t.charAt(i))
+    //             continue;
+    //         else
+    //             return false;
+    //     }
+    //     else
+    //     {
+    //         if(pair.containsValue(t.charAt(i)))
+    //             return false;
+    //         pair.put(s.charAt(i),t.charAt(i));
+    //     }
+    // }
+    // return true;
     }
 }
